e97f307dc952d4029b461f6fc61e8339676e5d01
Zhaosgithub <zhao.cmu@gmail.com>
2016-06-07 16:21:08 -0700

    Initial Commit

---
+++ week 4Tests/week_4Tests.swift
@@ -0,0 +1,36 @@
+//
+//  week_4Tests.swift
+//  week 4Tests
+//
+//  Created by Zhaochang He on 6/7/16.
+//  Copyright © 2016 Zhaochang He. All rights reserved.
+//
+
+import XCTest
+@testable import week_4
+
+class week_4Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
